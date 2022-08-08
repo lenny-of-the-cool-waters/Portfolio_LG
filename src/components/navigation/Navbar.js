efd4e385823bc5bbedca79b2f98b5c61d6c2467e
lenny-of-the-cool-waters <lennygith@gmail.com>
2022-08-08 09:37:28 +0300

    Fixed navbar styling

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/Navbar.css";
 
 const Navbar = () => {
+  const [isHamburgerActive, setIsHamburgerActive] = useState(false);
+  const [isMobileMenuActive, setIsMobileMenuActive] = useState(false);
+
+  const handleMenuState = () => {
+    setIsHamburgerActive(!isHamburgerActive);
+    setIsMobileMenuActive(!isMobileMenuActive);
+  };
+
+  useEffect(() => {
+    const scrollColorChanger = () => {
+      const header = document.querySelector(".header.container");
+      let scroll_position = window.scrollY;
+
+      if (scroll_position > 250) {
+        header.style.backgroundColor = "rgba(46, 46, 56, .99)";
+        header.style.backgroundImage =
+          "linear-gradient(135deg,rgb(30, 30, 34) 0%, rgb(58, 58, 58)100%)";
+      } else {
+        header.style.backgroundColor = "rgba(31, 30, 30, 0.1)";
+        header.style.backgroundImage = "none";
+      }
+    };
+
+    window.addEventListener("scroll", scrollColorChanger);
+
+    return () => {
+      window.removeEventListener("scroll", scrollColorChanger);
+    };
+  }, []);
+
   return (
     <header id="header">
       <div className="header container">
@@ -14,10 +44,13 @@ const Navbar = () => {
           </div>
 
           <div className="nav-list">
-            <div className="hamburger">
+            <div
+              className={`hamburger ${isHamburgerActive ? "active" : null}`}
+              onClick={handleMenuState}
+            >
               <div className="bar"></div>
             </div>
-            <ul>
+            <ul className={isMobileMenuActive ? "active" : null}>
               <li className="nav-item">
                 <a href="#hero" dataAfter="Home">
                   Home
