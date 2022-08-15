274f29db4b926b074401c543b51cd7172c66777d
lenny-of-the-cool-waters <lennygith@gmail.com>
2022-08-15 12:08:42 +0300

    Fixed contacts

--- src/sections/Contacts/Contacts.js
+++ src/sections/Contacts/Contacts.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 import './Contacts.css';
 
 const Contacts = () => {
@@ -38,8 +40,8 @@ const Contacts = () => {
             </div>
             <div className="contact-info">
               <h1>Socials</h1>
-              <h2><a className="social-link" href="https://www.linkedin.com/in/lenny-githenya/">LinkedIn</a></h2>
-              <h2><a className="social-link" href="https://github.com/lenny-of-the-cool-waters">Github</a></h2>
+              <h2><a className="social-link" href="https://www.linkedin.com/in/lenny-githenya/">LinkedIn</a> <FontAwesomeIcon icon={faLinkedin} /></h2>
+              <h2><a className="social-link" href="https://github.com/lenny-of-the-cool-waters">Github</a> <FontAwesomeIcon icon={faGithub} /></h2>
             </div>
           </div>
         </div>
--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './Projects.css';
+import "./Projects.css";
 import projectsList from "./Projects.json";
 import Project from "./Project";
 
@@ -13,10 +13,29 @@ const Projects = () => {
           </h1>
         </div>
         <div className="all-projects">
-        { projectsList.map(project => {
-          console.log(project);
-            return (<Project key={project.name} name={project.name} category={project.category} description={project.description} imagePath={project.imagePath}/>);
+          {projectsList.map((project) => {
+            return (
+              <Project
+                key={project.name}
+                name={project.name}
+                category={project.category}
+                description={project.description}
+                imagePath={project.imagePath}
+              />
+            );
           })}
+
+          <div className="project-item">
+            <div className="project-info">
+              <h1>More Good Stuff</h1>
+              <h2>Github repos</h2>
+              <p>Here are more projects I have worked on. They cover topics such as Frontend web development, Backend Web development, Database administration, Cloud computing, IoT and robotics.</p>
+              <a href="https://github.com/lenny-of-the-cool-waters" type="button" className="github-btn">My Github</a>
+            </div>
+            <div className="project-image">
+              <img src="/img/projects/github.jfif" alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </section>
