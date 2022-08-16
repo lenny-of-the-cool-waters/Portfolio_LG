1f4c9a0037517667c447cc343bf608deb4ffbf5c
lenny-of-the-cool-waters <lennygith@gmail.com>
2022-08-16 11:47:55 +0300

    Fixed resume download

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import './About.css';
-// import resume from './Resume-LennyGithenya.pdf';
+import { saveAs } from 'file-saver';
 
 const About = () => {
-    // const downloadCV = () => {
-    //     fetch("./Resume-LennyGithenya.pdf").then((response) => {
-    //       response.blob().then((blob) => {
-    //         // Creating new object of PDF file
-    //         const fileURL = window.URL.createObjectURL(blob);
-    //         // Setting various property values
-    //         let alink = document.createElement("a");
-    //         alink.href = fileURL;
-    //         alink.download = "LennyGithenya_Resume.pdf";
-    //         alink.click();
-    //       });
-    //     });
-    // }
+    const downloadCV = () => {
+        saveAs("./Resume-LennyGithenya.pdf", "LennyGithenya_CV.pdf");
+    }
 
   return (
     <section id="about">
@@ -29,7 +19,7 @@ const About = () => {
                 <h1 className="section-title">About <span>me</span></h1>
                 <h2>Full stack developer | IoT | Technical Support</h2>
                 <p>My name is Lenny Githenya and I am a fullstack web and IoT developer. My professional experience includes work with multinational organizations (Minet Kenya, TSC Kenya, NCBA Group) and freelance development projects. I believe that in a world full of dreamers no one should leave all their dreams in bed, and I put in the work to achieve this for me and those I work with. After a long day of work, I enjoy a good swim or getting lost in the pages of a book.</p>
-                <a href="./Resume-LennyGithenya.pdf" download><button className="cta">Download resume</button></a>
+                <button className="cta" onClick={downloadCV}>Download resume</button>
             </div>
         </div>
     </section>
