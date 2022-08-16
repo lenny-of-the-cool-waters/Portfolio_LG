3cfba5f9ed6d51bae0780d3e9257fe1f4f558af1
lenny-of-the-cool-waters <lennygith@gmail.com>
2022-08-16 11:54:14 +0300

    Resume bug fix

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import './About.css';
-import { saveAs } from 'file-saver';
 
 const About = () => {
-    const downloadCV = () => {
-        saveAs("./Resume-LennyGithenya.pdf", "LennyGithenya_CV.pdf");
-    }
-
   return (
     <section id="about">
         <div className="about container">
@@ -19,7 +14,7 @@ const About = () => {
                 <h1 className="section-title">About <span>me</span></h1>
                 <h2>Full stack developer | IoT | Technical Support</h2>
                 <p>My name is Lenny Githenya and I am a fullstack web and IoT developer. My professional experience includes work with multinational organizations (Minet Kenya, TSC Kenya, NCBA Group) and freelance development projects. I believe that in a world full of dreamers no one should leave all their dreams in bed, and I put in the work to achieve this for me and those I work with. After a long day of work, I enjoy a good swim or getting lost in the pages of a book.</p>
-                <button className="cta" onClick={downloadCV}>Download resume</button>
+                <a href="./Resume-LennyGithenya.pdf" download="LennyGithenya_CV.pdf"><button className="cta">Download resume</button></a>
             </div>
         </div>
     </section>
